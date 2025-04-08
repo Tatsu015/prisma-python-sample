4f4ee1a9edcd89a472b92547b0c208e3cc49a099
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-08 14:08:05 +0000

    add to use prisma sample

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from prisma-python-sample!")
+import asyncio
+from prisma import Prisma
 
+async def main() -> None:
+    prisma = Prisma()
+    await prisma.connect()
 
-if __name__ == "__main__":
-    main()
+    # write your queries here
+    user = await prisma.user.create(
+        data={
+            'name': 'Robert',
+            'email': 'robert@craigie.dev'
+        },
+    )
+
+    await prisma.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
