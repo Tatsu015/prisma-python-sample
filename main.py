113dd9078863c2673bfb60074c5777bf01cbf638
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-09 10:28:22 +0000

    add read db

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ async def main() -> None:
     prisma = Prisma()
     await prisma.connect()
 
-    # write your queries here
-    user = await prisma.user.create(
-        data={
-            'name': 'Robert',
+    read_user = await prisma.user.find_first(
+        where={
             'email': 'robert@craigie.dev'
-        },
+        }
     )
+
+    if read_user:
+        print(f'read user:{read_user}')
+
+    else:
+        add_user = await prisma.user.create(
+            data={
+                'name': 'Robert',
+                'email': 'robert@craigie.dev'
+            },
+        )
+    
+        print(f"add user:{add_user}")
+
 
     await prisma.disconnect()
 
