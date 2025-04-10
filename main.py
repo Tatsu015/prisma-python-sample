f6a05b88cce54de6be5db58b4d2e4d66acccdf1b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-10 13:15:41 +0000

    remove db class

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from prisma import Prisma
 from prisma.models import User
-from db import DB
 from contextlib import asynccontextmanager
 
 prisma = Prisma()
-db = DB(prisma)
 
 
 @asynccontextmanager
@@ -18,9 +16,19 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+@app.get('/users', response_model=list[User])
+async def get_users():
+    users = await prisma.user.find_many()
+    return users
+
+
 @app.get('/users/{email}', response_model=User)
 async def get_user(email: str):
-    user = await db.get_user(email)
+    user = await prisma.user.find_first(
+        where={
+            'email': email
+        }
+    )
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
@@ -29,7 +37,9 @@ async def get_user(email: str):
 @app.post('/users', response_model=User)
 async def create_user(email: str, name: str):
     try:
-        user = await db.add_user(email, name)
+        user = await prisma.user.create(
+            data={"email": email, "name": name}
+        )
         return user
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -37,7 +47,11 @@ async def create_user(email: str, name: str):
 
 @app.put('/users/{email}', response_model=User)
 async def update_user(email: str, new_name: str):
-    existing_user = await db.get_user(email)
+    existing_user = await prisma.find_first(
+        where={
+            'email': email
+        }
+    )
     if existing_user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
@@ -50,11 +64,15 @@ async def update_user(email: str, new_name: str):
 
 @app.delete('/users/{email}')
 async def delete_user(email: str):
-    existing_user = await db.get_user(email)
+    existing_user = await prisma.user.find_first(
+        where={
+            'email': email
+        }
+    )
     if existing_user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
     deleted_user = await prisma.user.delete(
         where={'email': email}
     )
-    return {"message": f"User with email {email} deleted successfully"}
+    return {"message": f"User with email {deleted_user} deleted successfully"}
