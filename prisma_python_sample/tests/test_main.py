6fa17bae5e712c43d942b22ae38bd0e66cfdc03a
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-12 16:34:57 +0000

    success to pass test

--- prisma_python_sample/tests/test_main.py
+++ prisma_python_sample/tests/test_main.py
@@ -1,22 +1,42 @@
 import pytest
+import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 from prisma_python_sample.main import router, get_prisma
 from fastapi import FastAPI
+from prisma import Prisma
 
 
-@pytest.mark.anyio
-async def test_root():
+@pytest_asyncio.fixture
+async def prisma() -> Prisma:
     prisma = get_prisma()
     await prisma.connect()
 
-    app = FastAPI()
+    yield prisma
 
+    await prisma.disconnect()
+
+
+@pytest_asyncio.fixture
+async def app() -> AsyncClient:
+    app = FastAPI()
     app.include_router(router)
 
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as ac:
-        response = await ac.get("/users")
-    assert response.status_code == 200
+        yield ac
 
-    await prisma.disconnect()
+
+@pytest.mark.asyncio
+async def test_get(prisma: Prisma, app: AsyncClient):
+    await prisma.user.create(
+        data={
+            "email": "test@gmail.com",
+            "name": "testuser",
+        }
+    )
+
+    response = await app.get("/users")
+    assert response.status_code == 200
+    j = response.json()
+    assert len(j) == 1
