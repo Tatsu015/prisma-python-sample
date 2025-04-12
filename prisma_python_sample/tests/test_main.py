f242ba6ba0dd001ffd73cd6ed9826c19829dec72
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-12 03:21:16 +0000

    success to pass test

--- prisma_python_sample/tests/test_main.py
+++ prisma_python_sample/tests/test_main.py
@@ -1,114 +1,22 @@
-from prisma_python_sample.main import app, get_prisma
 import pytest
-from fastapi.testclient import TestClient
-from prisma_python_sample.main import app, get_prisma
+from httpx import ASGITransport, AsyncClient
+from prisma_python_sample.main import router, get_prisma
+from fastapi import FastAPI
 
 
-# @pytest.fixture(scope="module")
-# def test_client():
-#     client = TestClient(app)
-#     yield client
-
-
-# @pytest.fixture(autouse=True, scope="module")
-# async def setup_db():
-#     prisma = get_prisma()
-#     await prisma.connect()
-#     await prisma.user.delete_many()
-#     yield
-#     await prisma.disconnect()
-
-
-@pytest.mark.asyncio
-async def test_create_and_get_user():
-    email = "test@example.com"
-    name = "Test User"
-
-    client = TestClient(app)
-
+@pytest.mark.anyio
+async def test_root():
     prisma = get_prisma()
     await prisma.connect()
-    # await prisma.user.delete_many()
 
-    # app.dependency_overrides[get_prisma] = lambda: prisma
+    app = FastAPI()
 
-    # response = await test_client.post(f"/users?email={email}&name={name}")
-    # assert response.status_code == 200
-    # created_user = response.json()
-    # assert created_user["email"] == email
-    # assert created_user["name"] == name
+    app.include_router(router)
 
-    # prisma.user.create.assert_called_once_with(
-    #     data={"email": email, "name": name})
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/users")
+    assert response.status_code == 200
 
-    # response = await test_client.get(f"/users/{email}")
-    # assert response.status_code == 200
-    # retrieved_user = response.json()
-    # assert retrieved_user["email"] == email
-    # assert retrieved_user["name"] == name
-
-    # await prisma.user.find_first.assert_called_once_with(where={'email': email})
-
-    # app.dependency_overrides = {}
-
-    # await prisma.disconnect()
-    assert True
-
-# def test_get_nonexistent_user(test_client: TestClient, mock_prisma):
-#     app.dependency_overrides[get_prisma] = lambda: mock_prisma
-
-#     response = test_client.get("/users/nonexistent@example.com")
-#     assert response.status_code == 404
-#     assert response.json()["detail"] == "User not found"
-
-#     mock_prisma.user.find_first.assert_called_once_with(
-#         where={'email': 'nonexistent@example.com'})
-
-#     app.dependency_overrides = {}
-
-
-# def test_get_users(test_client: TestClient, mock_prisma):
-#     app.dependency_overrides[get_prisma] = lambda: mock_prisma
-#     mock_prisma.user.find_many.return_value = []
-
-#     response = test_client.get("/users")
-#     assert response.status_code == 200
-#     assert response.json() == []
-
-#     mock_prisma.user.find_many.assert_called_once()
-
-#     app.dependency_overrides = {}
-
-
-# def test_update_user(test_client: TestClient, mock_prisma):
-#     email = "test@example.com"
-#     new_name = "New Test User"
-
-#     app.dependency_overrides[get_prisma] = lambda: mock_prisma
-#     mock_prisma.user.find_first.return_value = {
-#         "email": email, "name": "Old Name"}
-
-#     response = test_client.put(f"/users/{email}?new_name={new_name}")
-#     assert response.status_code == 200
-
-#     mock_prisma.user.find_first.assert_called_once_with(where={'email': email})
-#     mock_prisma.user.update.assert_called_once_with(
-#         where={'email': email}, data={'name': new_name})
-
-#     app.dependency_overrides = {}
-
-
-# def test_delete_user(test_client: TestClient, mock_prisma):
-#     email = "test@example.com"
-
-#     app.dependency_overrides[get_prisma] = lambda: mock_prisma
-#     mock_prisma.user.find_first.return_value = {
-#         "email": email, "name": "Test User"}
-
-#     response = test_client.delete(f"/users/{email}")
-#     assert response.status_code == 200
-
-#     mock_prisma.user.find_first.assert_called_once_with(where={'email': email})
-#     mock_prisma.user.delete.assert_called_once_with(where={'email': email})
-
-#     app.dependency_overrides = {}
+    await prisma.disconnect()
