a1c2f28502246573ec2208986a791d75763aa47b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-12 03:20:43 +0000

    remove warning

--- prisma_python_sample/main.py
+++ prisma_python_sample/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from prisma_python_sample.prisma import get_prisma
 from prisma_python_sample.roter import router
@@ -5,6 +7,7 @@ from prisma_python_sample.roter import router
 __prisma = get_prisma()
 
 
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     await __prisma.connect()
     yield
